879de5212e67575d3f96ff88c5fa859337face0e
pawel <wispawelwis38@gmail.com>
2020-11-01 21:58:59 +0100

    Gdansk can be webscrapped

--- BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
+++ BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
@@ -7,6 +7,8 @@ import re
 def run():
     webscrapKrakow()
     webscrapBialystok()
+    webscrapBydgoszcz()
+    webscrapGdansk()
 
 
 def webscrapKrakow():
@@ -19,7 +21,7 @@ def webscrapKrakow():
         'src': lambda x: x and re.match('https://rckik.krakow.pl/wp-content/uploads/2016/11/\d{1,3}.png', x)})
     for anchor in anchors:
         link = anchor['src']
-        volume = int(link[link.rfind(r'/') + 1:][:-4])
+        volume = int(link[link.rfind(r'/') + 1:link.rfind(r'.')])
         group = anchor.parent.next_sibling.find('strong').text.replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
         try:
@@ -53,6 +55,48 @@ def webscrapBialystok():
             br = BloodReservesModel.objects.get(region='Bialystok', group=group)
         except BloodReservesModel.DoesNotExist:
             BloodReservesModel(region="Bialystok", volume=volume, group=group).save()
+        else:
+            br.volume = volume
+            br.save()
+
+
+def webscrapBydgoszcz():
+    try:
+        webpage = requests.get(r"http://www.rckik-bydgoszcz.com.pl")
+    except ConnectionError:
+        return
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    a = soup.findAll('li', {'style': lambda x: x and x.startswith('background:url(theme/default/img')})
+    for x in a:
+        group = x['style'][x['style'].rfind('/') + 1:][:-6].replace('minus', ';').replace('plus', ';')
+        group = group.upper().replace('0', 'Z').split(';')
+        group, volume = group[0], 5 - int(group[1])
+        if volume == 1: volume = 0
+        rh = 'P' if "plus" in x['style'] else 'N'
+        group += f'_{rh}'
+        try:
+            br = BloodReservesModel.objects.get(region='Bydgoszcz', group=group)
+        except BloodReservesModel.DoesNotExist:
+            BloodReservesModel(region="Bydgoszcz", volume=volume, group=group).save()
+        else:
+            br.volume = volume
+            br.save()
+
+def webscrapGdansk():
+    try:
+        webpage = requests.get(r"http://krew.gda.pl")
+    except ConnectionError:
+        return
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    a = soup.findAll('img', {'src': lambda x: x and re.match('images/blood_\d.png', x)})
+    for x in a:
+        volume = 5 - int(x['src'][x['src'].rfind('.') - 1])
+        group = x.parent.next_sibling.text.replace(u'\xa0', ' ')
+        group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
+        try:
+            br = BloodReservesModel.objects.get(region='Gdansk', group=group)
+        except BloodReservesModel.DoesNotExist:
+            BloodReservesModel(region="Gdansk", volume=volume, group=group).save()
         else:
             br.volume = volume
             br.save()
