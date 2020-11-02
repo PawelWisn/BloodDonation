d9474cd9f4612c2c199dc188e997a45904f6b6da
pawel <wispawelwis38@gmail.com>
2020-11-02 17:19:51 +0100

    Olsztyn can be webscrapped

--- BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
+++ BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
@@ -14,7 +14,8 @@ def run():
     # webscrapGdansk()
     # webscrapKatowice()
     # webscrapKielce()
-    webscrapLublin()
+    # webscrapLublin()
+    webscrapOlsztyn()
 
 
 def webscrapKrakow():
@@ -28,7 +29,7 @@ def webscrapKrakow():
     for anchor in anchors:
         link = anchor['src']
         volume = int(link[link.rfind(r'/') + 1:link.rfind(r'.')])
-        group = anchor.parent.next_sibling.find('strong').text.replace(u'\xa0', ' ')
+        group = anchor.parent.next_sibling.find('strong').text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
         try:
             br = BloodReservesModel.objects.get(region='Krakow', group=group)
@@ -54,7 +55,7 @@ def webscrapBialystok():
             volume = 2
         elif volume == 'max':
             volume = 4
-        group = x.text.replace(u'\xa0', ' ')
+        group = x.text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
         try:
             br = BloodReservesModel.objects.get(region='Bialystok', group=group)
@@ -74,10 +75,10 @@ def webscrapBydgoszcz():
     a = soup.findAll('li', {'style': lambda x: x and x.startswith('background:url(theme/default/img')})
     for x in a:
         group = x['style'][x['style'].rfind('/') + 1:][:-6].replace('minus', ';').replace('plus', ';')
-        group = group.upper().replace('0', 'Z').split(';')
+        group = group.strip().upper().replace('0', 'Z').split(';')
         group, volume = group[0], 5 - int(group[1])
         if volume == 1: volume = 0
-        rh = 'P' if "plus" in x['style'] else 'N'
+        rh = ('P' if "plus" in x['style'] else 'N')
         group += f'_{rh}'
         try:
             br = BloodReservesModel.objects.get(region='Bydgoszcz', group=group)
@@ -97,7 +98,7 @@ def webscrapGdansk():
     a = soup.findAll('img', {'src': lambda x: x and re.match('images/blood_\d.png', x)})
     for x in a:
         volume = 5 - int(x['src'][x['src'].rfind('.') - 1])
-        group = x.parent.next_sibling.text.replace(u'\xa0', ' ')
+        group = x.parent.next_sibling.text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
         try:
             br = BloodReservesModel.objects.get(region='Gdansk', group=group)
@@ -117,13 +118,13 @@ def webscrapKatowice():
     chrome_options.experimental_options["prefs"] = chrome_prefs
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get('https://rckik-katowice.pl')
+    driver.get(r'https://rckik-katowice.pl')
     time.sleep(5)
     data = driver.find_element_by_class_name('drops').text.replace(u'\xa0', ' ').replace('stan ', '')
     data = data.replace('średni', '2').replace('niski', '1').replace('optymalny', '3')
     data = data.replace('maksymalny', '4').replace('krytyczny', '0').split('\n')
     for i in range(0, len(data), 2):
-        group = data[i].split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in data[i] else 'N')
+        group = data[i].strip().split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in data[i] else 'N')
         try:
             br = BloodReservesModel.objects.get(region='Katowice', group=group)
         except BloodReservesModel.DoesNotExist:
@@ -145,7 +146,7 @@ def webscrapKielce():
     for x in a:
         volume = int(x['src'][x['src'].rfind('/') + 1:][:2])
         if volume == 10: volume = 100
-        group = x.get('title')
+        group = x.get('title').strip()
         group = group.replace(u'\xa0', ' ').split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
         try:
             br = BloodReservesModel.objects.get(region='Kielce', group=group)
@@ -158,7 +159,7 @@ def webscrapKielce():
 
 def webscrapLublin():
     def getRh(x):
-        return 'P' if '+' in x.parent.parent.parent.parent.parent.find('h2').text else 'N'
+        return ('P' if '+' in x.parent.parent.parent.parent.parent.find('h2').text else 'N')
 
     try:
         webpage = requests.get(r"http://www.rckik.lublin.pl")
@@ -180,6 +181,36 @@ def webscrapLublin():
             br = BloodReservesModel.objects.get(region='Lublin', group=group)
         except BloodReservesModel.DoesNotExist:
             BloodReservesModel(region="Lublin", volume=volume, group=group).save()
+        else:
+            br.volume = volume
+            br.save()
+
+
+def webscrapOlsztyn():
+    try:
+        webpage = requests.get(r"http://rckikol.pl/potrzeba-krwi/")
+    except ConnectionError:
+        return
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    a = soup.findAll('strong')
+    a = list(filter(lambda x: re.search(r'^(0|A|B|AB)Rh\+?$', str(x.text.strip())), a))
+    for x in a:
+        group = x.text.strip().replace('0', 'Z').split('Rh')[0] + '_' + ('P' if '+' in x.text else 'N')
+        parent = x.parent.parent
+        volume = parent.find('div', {'data-fill-amount': lambda y: y and re.match(r'^\d{1,3}$', y)})['data-fill-amount']
+        volume = int(volume)
+        if volume >= 90:
+            volume = 4
+        elif volume >= 40:
+            volume = 2
+        elif volume >= 10:
+            volume = 1
+        else:
+            volume = 0
+        try:
+            br = BloodReservesModel.objects.get(region='Olsztyn', group=group)
+        except BloodReservesModel.DoesNotExist:
+            BloodReservesModel(region="Olsztyn", volume=volume, group=group).save()
         else:
             br.volume = volume
             br.save()
