d87764272b3d3ebd2a0838c05f51ac6891b52a54
pawel <wispawelwis38@gmail.com>
2020-11-02 18:01:28 +0100

    Poznan can be webscrapped

--- BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
+++ BloodDonationBackend/Main/scripts/webscrapBloodReserves.py
@@ -17,7 +17,17 @@ def run():
     # webscrapLublin()
     # webscrapOlsztyn()
     # webscrapOpole()
-    webscrapPoznan()
+    # webscrapPoznan()
+    webscrapRaciborz()
+
+def saveToDB(region, volume, group):
+    try:
+        br = BloodReservesModel.objects.get(region=region, group=group)
+    except BloodReservesModel.DoesNotExist:
+        BloodReservesModel(region=region, volume=volume, group=group).save()
+    else:
+        br.volume = volume
+        br.save()
 
 
 def webscrapKrakow():
@@ -33,13 +43,7 @@ def webscrapKrakow():
         volume = int(link[link.rfind(r'/') + 1:link.rfind(r'.')])
         group = anchor.parent.next_sibling.find('strong').text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
-        try:
-            br = BloodReservesModel.objects.get(region='Krakow', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Krakow", volume=volume // 25, group=group).save()
-        else:
-            br.volume = volume // 25
-            br.save()
+        saveToDB('Krakow',volume//25,group)
 
 
 def webscrapBialystok():
@@ -59,13 +63,7 @@ def webscrapBialystok():
             volume = 4
         group = x.text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
-        try:
-            br = BloodReservesModel.objects.get(region='Bialystok', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Bialystok", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Bialystok', volume, group)
 
 
 def webscrapBydgoszcz():
@@ -82,13 +80,7 @@ def webscrapBydgoszcz():
         if volume == 1: volume = 0
         rh = ('P' if "plus" in x['style'] else 'N')
         group += f'_{rh}'
-        try:
-            br = BloodReservesModel.objects.get(region='Bydgoszcz', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Bydgoszcz", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Bydgoszcz', volume, group)
 
 
 def webscrapGdansk():
@@ -102,13 +94,7 @@ def webscrapGdansk():
         volume = 5 - int(x['src'][x['src'].rfind('.') - 1])
         group = x.parent.next_sibling.text.strip().replace(u'\xa0', ' ')
         group = group.split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
-        try:
-            br = BloodReservesModel.objects.get(region='Gdansk', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Gdansk", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Gdansk', volume, group)
 
 
 def webscrapKatowice():
@@ -127,13 +113,7 @@ def webscrapKatowice():
     data = data.replace('maksymalny', '4').replace('krytyczny', '0').split('\n')
     for i in range(0, len(data), 2):
         group = data[i].strip().split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in data[i] else 'N')
-        try:
-            br = BloodReservesModel.objects.get(region='Katowice', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Katowice", volume=data[i + 1], group=group).save()
-        else:
-            br.volume = data[i + 1]
-            br.save()
+        saveToDB('Katowice', data[i + 1], group)
     driver.close()
 
 
@@ -150,13 +130,7 @@ def webscrapKielce():
         if volume == 10: volume = 100
         group = x.get('title').strip()
         group = group.replace(u'\xa0', ' ').split(' ')[0].replace('0', 'Z') + '_' + ('P' if '+' in group else 'N')
-        try:
-            br = BloodReservesModel.objects.get(region='Kielce', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Kielce", volume=volume // 25, group=group).save()
-        else:
-            br.volume = volume // 25
-            br.save()
+        saveToDB('Kielce', volume//25, group)
 
 
 def webscrapLublin():
@@ -179,13 +153,7 @@ def webscrapLublin():
         else:
             volume = 0
         group = x.parent.text.strip().replace('0', 'Z') + '_' + getRh(x)
-        try:
-            br = BloodReservesModel.objects.get(region='Lublin', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Lublin", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Lublin', volume, group)
 
 
 def webscrapOlsztyn():
@@ -209,13 +177,7 @@ def webscrapOlsztyn():
             volume = 1
         else:
             volume = 0
-        try:
-            br = BloodReservesModel.objects.get(region='Olsztyn', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Olsztyn", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Olsztyn', volume, group)
 
 
 def webscrapOpole():
@@ -229,13 +191,7 @@ def webscrapOpole():
         group = x['alt'].replace('0', 'Z').split(' ')[0] + '_' + ('P' if 'plus' in x['alt'] else 'N')
         volume = 5 - int(x['src'][x['src'].rfind(r'.') - 1])
         if volume <= 2: volume -= 1
-        try:
-            br = BloodReservesModel.objects.get(region='Opole', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Opole", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Opole', volume, group)
 
 
 def webscrapPoznan():
@@ -253,10 +209,23 @@ def webscrapPoznan():
         elif volume == 'L': volume=1
         else: volume=0
         print(group, volume)
-        try:
-            br = BloodReservesModel.objects.get(region='Poznan', group=group)
-        except BloodReservesModel.DoesNotExist:
-            BloodReservesModel(region="Poznan", volume=volume, group=group).save()
-        else:
-            br.volume = volume
-            br.save()
+        saveToDB('Poznan', volume, group)
+
+
+
+def webscrapRaciborz():
+    try:
+        webpage = requests.get(r"https://rckik.pl")
+    except ConnectionError:
+        return
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    a = soup.findAll('li', string=re.compile('^(A|0|B|AB) Rh [-+]$'))
+    for x in a:
+        group = x.text.replace('0', 'Z').split(' ')[0] + '_' + ('P' if '+' in x.text else 'N')
+        volume = x['class'][0][1:]
+        if volume == 'F': volume=4
+        elif volume == 'M': volume=2
+        elif volume == 'L': volume=1
+        else: volume=0
+        print(group, volume)
+        saveToDB('Raciborz', volume, group)
