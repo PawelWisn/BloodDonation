c71114a2d38464d64cfb349c7ce96c8a4d43827a
pawel <wispawelwis38@gmail.com>
2020-11-07 23:07:39 +0100

    basic tiles added to homepage

--- blood-donation-frontend/src/components/Homepage.tsx
+++ blood-donation-frontend/src/components/Homepage.tsx
@@ -12,19 +12,26 @@ import School from "../images/school.png"
 import RegDon from "../images/register_donation.png"
 import Trophy from "../images/trophy.png"
 import './Common.scss';
+import './Homepage.scss';
 
 function Homepage() {
     return (
         <div className="flex-wrapper">
             <UpperBar/>
-                <Tile subject="ELO" alt="elo" picture={Map}/>
-                <Tile subject="ELO" alt="elo" picture={BlRes}/>
-                <Tile subject="ELO" alt="elo" picture={Quiz}/>
-                <Tile subject="ELO" alt="elo" picture={Law}/>
-                <Tile subject="ELO" alt="elo" picture={Person}/>
-                <Tile subject="ELO" alt="elo" picture={School}/>
-                <Tile subject="ELO" alt="elo" picture={RegDon}/>
-                <Tile subject="ELO" alt="elo" picture={Trophy}/>
+            <div className='homepage-row'>
+                <Tile subject="BLOOD RESERVES" alt="storage" picture={BlRes}/>
+                <Tile subject="COLLECTION POINT" alt="map" picture={Map}/>
+                <Tile subject="DONOR TEST" alt="quiz" picture={Quiz}/>
+                <Tile subject="BENEFITS" alt="trophy" picture={Trophy}/>
+
+            </div>
+            <div className='homepage-row'>
+                <Tile subject="KNOWLEDGE" alt="education" picture={School}/>
+                <Tile subject="LEGAL REGULATIONS" alt="law" picture={Law}/>
+                <Tile subject="REGISTER DONATION" alt="register" picture={RegDon}/>
+                <Tile subject="MY PROFILE" alt="user" picture={Person}/>
+            </div>
+
             <BottomBar/>
         </div>
     );
