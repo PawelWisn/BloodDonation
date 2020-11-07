6e8e45f52083302485115d718b2809564c81c5e1
pawel <wispawelwis38@gmail.com>
2020-11-07 22:43:02 +0100

    footer goes down

--- blood-donation-frontend/src/components/Homepage.tsx
+++ blood-donation-frontend/src/components/Homepage.tsx
@@ -2,11 +2,29 @@ import Paper from '@material-ui/core/Paper';
 import UpperBar from './UpperBar';
 import BottomBar from "./BottomBar";
 import React from "react";
+import Tile from "./Tile";
+import Map from "../images/map.png"
+import BlRes from "../images/blood_reserves.png"
+import Quiz from "../images/quiz.png"
+import Law from "../images/law.png"
+import Person from "../images/person.png"
+import School from "../images/school.png"
+import RegDon from "../images/register_donation.png"
+import Trophy from "../images/trophy.png"
+import './Common.scss';
 
 function Homepage() {
     return (
-        <div>
+        <div className="flex-wrapper">
             <UpperBar/>
+                <Tile subject="ELO" alt="elo" picture={Map}/>
+                <Tile subject="ELO" alt="elo" picture={BlRes}/>
+                <Tile subject="ELO" alt="elo" picture={Quiz}/>
+                <Tile subject="ELO" alt="elo" picture={Law}/>
+                <Tile subject="ELO" alt="elo" picture={Person}/>
+                <Tile subject="ELO" alt="elo" picture={School}/>
+                <Tile subject="ELO" alt="elo" picture={RegDon}/>
+                <Tile subject="ELO" alt="elo" picture={Trophy}/>
             <BottomBar/>
         </div>
     );
---
+++ blood-donation-frontend/src/components/Tile.tsx
@@ -0,0 +1,25 @@
+// import Paper from "@material-ui/core/Paper";
+import React from "react";
+import './Tile.scss';
+
+
+function Tile(props: any) {
+    const {alt, subject, picture} = props;
+
+    return (
+        <div className='tile'>
+            <div className='icon'>
+                <img alt={alt} src={picture}/>
+            </div>
+            <div className='subject'>
+                <h2>{subject}</h2>
+            </div>
+
+        </div>
+
+    );
+}
+
+
+export default Tile;
+
