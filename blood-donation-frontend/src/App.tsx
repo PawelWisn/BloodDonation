0fe55281ef1948236b631f884f944013b898f322
pawel <wispawelwis38@gmail.com>
2020-11-07 20:09:34 +0100

    created Homepage.tsx

--- blood-donation-frontend/src/App.tsx
+++ blood-donation-frontend/src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import UpperBar from './components/UpperBar';
-import BottomBar from "./components/BottomBar";
+import Homepage from './components/Homepage'
 
 function App() {
-  return (
-    <div className="App">
-      <UpperBar></UpperBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <BottomBar></BottomBar>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Homepage/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ blood-donation-frontend/src/components/Homepage.tsx
@@ -0,0 +1,17 @@
+import Paper from '@material-ui/core/Paper';
+import UpperBar from './UpperBar';
+import BottomBar from "./BottomBar";
+import React from "react";
+
+function Homepage() {
+    return (
+        <div>
+            <UpperBar/>
+            <BottomBar/>
+        </div>
+    );
+
+}
+
+
+export default Homepage
