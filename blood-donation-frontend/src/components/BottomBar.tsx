7485c457ec6af55cae58cdb064321a4938d95a43
pawel <wispawelwis38@gmail.com>
2020-11-09 22:54:22 +0100

    added support us popup

--- blood-donation-frontend/src/components/BottomBar.tsx
+++ blood-donation-frontend/src/components/BottomBar.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './BottomBar.scss';
+import PopupButton from './PopupButton';
 
 function BottomBar() {
 
     return (
         <div className='bottom-bar flex-container-bottom-bar'>
-            <div className='bottom-item'><p>Copyright &copy; 2020 BloodDonation</p></div>
-            <div className='bottom-item'><button>Support Us</button></div>
-            <div className='bottom-item'><a href="mailto:blooddonation@yahoo.com">Send Feedback</a></div>
+            <div className='bottom-item'>
+                <p>Copyright &copy; 2020 BloodDonation</p>
+            </div>
+            <div className='bottom-item'>
+                <PopupButton message='Support Us'
+                             popupMessage={"Our website was created for the noble idea of saving human health and life, and not for profit, therefore we do not display any advertisements. If you want to help us maintain our website, we encourage you to support us in the form of a voluntary donation to our bank account: 14 1140 2004 0000 3202 7599 4501"}/>
+            </div>
+            <div className='bottom-item'>
+                <a href="mailto:blooddonation@yahoo.com">Send Feedback</a>
+            </div>
         </div>
     );
 }
