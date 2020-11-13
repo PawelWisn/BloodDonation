b8852cd33970dea951c5f4ab88bfbd535d61c327
pawel <wispawelwis38@gmail.com>
2020-11-13 11:53:26 +0100

    added graphlql lib

--- blood-donation-frontend/src/App.tsx
+++ blood-donation-frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.scss';
 import MainRouter from "./routes/Router";
+import { createClient, Provider } from 'urql';
+
+const client = createClient({ url: 'localhost:8000/graphql' });
 
 function App() {
     return (
+        <Provider value={client}>
         <div className="App">
-
             <MainRouter/>
-
         </div>
+        </Provider>
     );
 }
 
