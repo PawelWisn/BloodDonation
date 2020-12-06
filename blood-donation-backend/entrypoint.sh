1aabbad920fb0f298ef73f42a61534c88346380d
pawel <wispawelwis38@gmail.com>
2020-12-07 00:49:25 +0100

    tidy django structure

--- blood-donation-backend/entrypoint.sh
+++ blood-donation-backend/entrypoint.sh
@@ -9,7 +9,6 @@ python manage.py migrate
 echo "from Main.models import UserModel; UserModel.objects.create_superuser('admin@yahoo.com', 'admin');" | python manage.py shell
 
 python manage.py runscript uploadLocsToDB
-
-#python manage.py runscript sendEmailReminders
+python manage.py runscript sendEmailReminders
 python manage.py runscript webScrapBloodReserves
 exec "$@"
